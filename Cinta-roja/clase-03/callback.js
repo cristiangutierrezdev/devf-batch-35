896bed177faf14edf37a532bc8687ed79d031972
Cristian Gutierrez <cristiandisenadorvisual@gmail.com>
2019-10-25 21:54:52 -0500

    ejercicios agregados

--- Cinta-roja/clase-03/callback.js
+++ Cinta-roja/clase-03/callback.js
@@ -16,48 +16,79 @@
 
 
 
-function peticion() {
-    const users = [
-        {
-            name: "Cristian",
-            id: 1
-        },
-        {
-            name: "Carlos",
-            id: 2
-        },
-        {
-            name: "Gabo",
-            id: 3
-        }
-    ]
-    return users
+// function peticion() {
+//     const users = [
+//         {
+//             name: "Cristian",
+//             id: 1
+//         },
+//         {
+//             name: "Carlos",
+//             id: 2
+//         },
+//         {
+//             name: "Gabo",
+//             id: 3
+//         }
+//     ]
+//     return users
+// }
+
+
+// function getUsers (funcion){
+
+//     let datos = funcion() 
+//     return datos 
+// }
+
+// // console.log(getUsers(peticion))
+
+
+// function showName(name){
+//     return name
+// }
+
+// function showLastName(lastName){
+//     return lastName
+// }
+
+
+// function getFullName ( isName, isLastName){
+//     let name = showName(isName)
+//     let lastName = showLastName(isLastName)
+
+//     return console.log(`${name} ${lastName}`) 
+// }
+
+// getFullName("Cristian", "Gutierrez")
+
+
+
+
+function suma(a, b) {
+    let suma = a + b
+
+    return console.log(suma)
 }
 
+// suma(2,5)
 
-function getUsers (funcion){
-    
-    let datos = funcion() 
-    return datos 
+function activadora(funcion, a,b) {
+   return funcion(a,b)
 }
 
-// console.log(getUsers(peticion))
+activadora(suma, 2, 4)
 
 
-function showName(name){
-    return name
-}
+// function maquina(moneda, producto) {
+//     if (moneda === 500 && producto === "Chocorramo") {
+//         return console.log( "Chocorramo")
+//     } else if (moneda === 1000 && producto === "Pollo") {
+//         return console.log("Pollo")
+//     } else {
+//         return console.log("Error")
+//     }
+// }
 
-function showLastName(lastName){
-    return lastName
-}
 
-
-function getFullName ( isName, isLastName){
-    let name = showName(isName)
-    let lastName = showLastName(isLastName)
-
-    return console.log(`${name} ${lastName}`) 
-}
-
-getFullName("Cristian", "Gutierrez")
+// maquina(1000, "Pollo")
